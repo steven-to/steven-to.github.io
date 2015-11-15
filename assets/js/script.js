4cf46291b5a1f9f14d704ee8bd22cafc1147dbcd
Steven To <steven.edto@gmail.com>
2015-11-15 21:45:26 +0700

    v0.9 (beta) is finished.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,54 @@ $(document).ready(function(){
     $('.navbar ul').toggleClass('showing');
   });
 
-  var aboutFromTop = $('#about').offset().top;
+  var aboutFromTop = $("#about").offset().top;
+  var blogPreviewFromTop = $('#blog-preview').offset().top;
+  var worksFromTop = $('#works').offset().top;
+  var connectFromTop = $('#connect').offset().top;
 
-  $(window).scroll(function() {
-    var scrollP = $(this).scrollTop();
-    $('#about').css('top', -scrollP/2);
-    $('#blog-preview').css('top', -scrollP/2);
+  $(".navbar a").on('click', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    $(".navbar ul li").find('a.active').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $(".about-nav").on('click', function(event) {
+    event.preventDefault();
+    $('body').animate({scrollTop : aboutFromTop - 150}, 500);
+  });
+
+  $(".blog-nav").on('click', function(event) {
+    event.preventDefault();
+    $('body').animate({scrollTop : blogPreviewFromTop}, 500);
+  });
+
+  $(".works-nav").on('click', function(event) {
+    event.preventDefault();
+    $('body').animate({scrollTop : worksFromTop}, 500);
+  });
+
+  $(".connect-nav").on('click', function(event) {
+    event.preventDefault();
+    $('body').animate({scrollTop : connectFromTop}, 500);
+  });
+
+
+
+
+  // $(window).scroll(function() {
+  //   var scrollP = $(this).scrollTop();
+  //   $('#about').css('top', -scrollP/4);
+  //   $('#blog-preview').css('top', -scrollP/3.5);
+  //   $('#works').css('top', -scrollP/3);
+  //   $('#connect').css('top', -scrollP/2.5);
+  // });
+
+  // Animate article's meta when the thumbnail is hovered
+  $('.article-preview-thumb').hover(function() {
+    $(this).find('.article-preview-meta').animate({top: '0'}, 200);
+  }, function() {
+    $(this).find('.article-preview-meta').animate({top: '100%'}, 200);
   });
 
 });
